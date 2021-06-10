292adeeb99b542f74d3e583c4705ee51c85f93a5
Kevin Kelly <kelly1@protonmail.com>
2021-06-10 01:22:14 -0700

    Scroll Up Button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-/*====================================================
+/*============================================================
 
-    jQuery Examples From YouTube Tutorials
     Source Code From Prakash Shahi @ codingnepalweb.com
+    3 Tutorials: Sticky Header, Toggle Menu, Scroll-Up Button
+    Source: https://www.youtube.com/watch?v=tcskp-ncN0I
 
-======================================================*/
+==============================================================*/
 
 $(document).ready(function() {
 
-  // Sticky Navbar Script
+  // (1) Sticky Navbar Script
   $(window).scroll(function() {
     if(this.scrollY > 20) {
       $('.navbar').addClass("sticky");
     } else {
       $('.navbar').removeClass("sticky");
     }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
   });
 
-  // Toggle Menu Navbar Script From Prakash Shahi @ codingnepalweb.com
+  // (2) "Slide Up" Button Script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+  });
+
+  // (3) Toggle Menu Navbar Script From Prakash Shahi @ codingnepalweb.com
   $('.hamburger-menu').click(function(){
       $('.navbar .menu').toggleClass("active"); // bring menu into view
       $('.hamburger-menu i').toggleClass("active"); // change hamburger menu icon to an "x"
@@ -27,19 +38,18 @@ $(document).ready(function() {
 
 
 
-/*===================================================
+/*===============================================================
 
     PHOTO GALLERY
-    Code From Class, Week 10
+    Reconfigured Code From Class, Week 10
     (International Space Station Exercise)
 
-=====================================================*/
+=================================================================*/
 
 let htmlCode = "";
 
 
 // Attach JPEGS to htmlCode variable
-
 for (let i = 1; i <= 24; i++){
   htmlCode += "<figure>";
   htmlCode += "<img alt='Pliocene Shark Fossil of the Pico Formation' src='images/LACMIP_42879-" + i + ".jpg' />";
@@ -47,7 +57,7 @@ for (let i = 1; i <= 24; i++){
   htmlCode += "</figure>";
 }
 
-// Add variable to the page
+// Add htmlCode variable into the page
 document.getElementById("gallery").innerHTML = htmlCode;
 
 
@@ -55,6 +65,7 @@ document.getElementById("gallery").innerHTML = htmlCode;
 /*===================================================
 
     Add YEAR to the Footer
+    Source Code from StackOverflow: https://stackoverflow.com/a/28627098
 
 =====================================================*/
 document.getElementById("year").innerHTML = new Date().getFullYear();
